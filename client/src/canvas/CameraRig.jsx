ff10ae23b369ed3a82e92b423ef9f0430d41fa72
amar.singh <amar.singh@kuehne-nagel.com>
2023-04-22 22:58:51 +0300

    shirt model improvements

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -9,7 +9,31 @@ const CameraRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
-  return <group>{children}</group>;
+  useFrame((state, delta) => {
+    const isBreakPoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    /* set initial position */
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakPoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    /* set camera position */
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    /* set model rotation */
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
