e87f12861edd1fc2d2d003bca90814716bb0caa9
amar.singh <amar.singh@kuehne-nagel.com>
2023-04-22 14:39:06 +0300

    homepage animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
+import state from "../store";
+
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -9,7 +11,42 @@ import {
 } from "../config/motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                DESIGN <br className="xl:block hidden" />
+                YOUR <br className="xl:block hidden" />
+                TSHIRT
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600">
+                Bring your t-shirt ideas to life with our 3D design tool!{" "}
+                <strong>Design, visualize, and create</strong> your perfect
+                t-shirt
+              </p>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
